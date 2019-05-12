0fcc3814a6ee03e0ad64f42973e44e8a38c6b937
David Bragmark <david.bragmark@valtech.com>
2019-05-12 21:09:00 +0200

    Auth for github implemented

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
+require('dotenv').config();
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const expressPlayground = require('graphql-playground-middleware-express')
 	.default;
-
+const { MongoClient } = require('mongodb');
 const { readFileSync } = require('fs');
 const resolvers = require('./resolvers');
 const typeDefs = readFileSync('./typedef.graphql', 'UTF-8');
 
-const app = new express();
+async function start() {
+	const app = new express();
 
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-});
+	const MONGO_DB = process.env.DB_URL;
+	const client = await MongoClient.connect(MONGO_DB, {
+		useNewUrlParser: true,
+		autoReconnect: true,
+	});
+	const db = client.db();
+	const context = { db };
 
-server.applyMiddleware({ app });
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		context: async ({ req }) => {
+			const githubToken = req.headers.authorization;
+			const currentUser = await db.collection('users').findOne({ githubToken });
+			return { db, currentUser };
+		},
+	});
 
-app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+	server.applyMiddleware({ app });
 
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+	app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
 
-app.listen({ port: 4000 }, () =>
-	console.log(
-		`GraphQL Server running  http://localhost:4000${server.graphqlPath}`,
-	),
-);
+	app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+	app.listen({ port: 4000 }, () =>
+		console.log(
+			`GraphQL Server running  http://localhost:4000${server.graphqlPath}`,
+		),
+	);
+}
+start();
--- resolvers.js
+++ resolvers.js
@@ -1,50 +1,61 @@
 const { GraphQLScalarType } = require('graphql');
-var tags = [
-	{ photoID: '1', userID: 'gPlake' },
-	{ photoID: '2', userID: 'sSchmidt' },
-	{ photoID: '2', userID: 'mHattrup' },
-	{ photoID: '2', userID: 'gPlake' },
-];
-
-var users = [
-	{ githubLogin: 'mHattrup', name: 'Mike Hattrup' },
-	{ githubLogin: 'gPlake', name: 'Glen Plake' },
-	{ githubLogin: 'sSchmidt', name: 'Scott Schmidt' },
-];
-
-var _id = 0;
-var photos = [
-	{
-		id: '1',
-		name: 'Dropping the Hart Chute',
-		description: 'The heart chute is one of my favorite chutes',
-		category: 'ACTION',
-		githubUser: 'gPlake',
-		created: '3-28-1977',
-	},
-	{
-		id: '2',
-		name: 'Enjoying the sunshine',
-		category: 'SELFIE',
-		githubUser: 'sSchmidt',
-		created: '1-2-1985',
-	},
-	{
-		id: '3',
-		name: 'Gunbarrel 25',
-		description: '25 laps on gunbarrel today',
-		category: 'LANDSCAPE',
-		githubUser: 'sSchmidt',
-		created: '2018-04-15T19:01:49.308Z',
-	},
-];
+const fetch = require('node-fetch');
+const { ObjectID } = require('mongodb');
+const { authorizeWithGithub } = require('./lib');
 
 module.exports = {
 	Query: {
-		totalPhotos: () => photos.length,
-		allPhotos: () => photos,
+		me: (parent, args, { currentUser }) => currentUser,
+
+		totalPhotos: (parent, args, { db }) =>
+			db.collection('photos').estimatedDocumentCount(),
+
+		allPhotos: (parent, args, { db }) =>
+			db
+				.collection('photos')
+				.find()
+				.toArray(),
+
+		totalUsers: (parent, args, { db }) =>
+			db.collection('users').estimatedDocumentCount(),
+
+		allUsers: (parent, args, { db }) =>
+			db
+				.collection('users')
+				.find()
+				.toArray(),
 	},
 	Mutation: {
+		async githubAuth(parent, { code }, { db }) {
+			let {
+				message,
+				access_token,
+				avatar_url,
+				login,
+				name,
+			} = await authorizeWithGithub({
+				client_id: process.env.CLIENT_ID,
+				client_secret: process.env.CLIENT_SECRET,
+				code,
+			});
+			if (message) {
+				throw new Error(message);
+			}
+			let latestUserInfo = {
+				name,
+				githubLogin: login,
+				githubToken: access_token,
+				avatar: avatar_url,
+			};
+			const {
+				ops: [user],
+			} = await db
+				.collection('users')
+				.replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true });
+
+			return { user, token: access_token };
+		},
+
 		postPhoto(parent, args) {
 			var newPhoto = {
 				id: _id++,
